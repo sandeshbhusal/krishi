919e579dcb1dbec65f48e1acf10a62846dff7811
Sandesh Bhusal <sandeshbhusal@MacBook-Air.local>
2020-05-17 11:00:47 +0545

    Fixed changes.

--- krishiapp/urls.py
+++ krishiapp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
     path('', views.indexView, name = "homepage"),
-    path('secondpage/', views.secondPage, name = "secondpage")
+    path('secondpage/', views.secondPage, name = "secondpage"),
     path('cropslist', views.showCropsList)
 ]
