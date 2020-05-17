8b9f15717bcb80ccd98f948531ef21e67d8d9ec7
root <root@localhost.localdomain>
2020-05-17 10:40:01 +0545

    Added crops list page with some elemets

--- krishiapp/views.py
+++ krishiapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def indexView(request):
     return render(request, "index.html", {})
+
+def showCropsList(request):
+	return render(request, "cropslist.html", {})
