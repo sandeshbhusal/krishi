8eb51d7b009741a80f64bf23ffac8d25a5aa376b
PrasangaDhungel <dhungel611@gmail.com>
2020-05-17 09:46:07 +0545

    started app

--- krishiapp/admin.py
+++ krishiapp/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Crops
 # Register your models here.
-admin.site.Register([Crops])
-=======
-
-# Register your models here.
->>>>>>> 183b360e2a4d3a4ae1de9786b8f2751b7b3b6de1
+admin.site.register([Crops])
--- krishiapp/models.py
+++ krishiapp/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Crops(models.Model):
-    cropName     = models.Charfield(max_length = 30)
+    cropName     = models.CharField(max_length = 30)
     altitude     = models.IntegerField()
     humidity     = models.IntegerField()
     temperature  = models.FloatField()
-    manpower     = models.Charfield(max_length = 100)
-    fertilizer   = models.Charfield(max_length = 100)
-    insecticides = models.Charfield(max_length = 50)
-    pesticides   = models.Charfield(max_length = 50)
-    investment   = models.Charfield(max_length = 50)
-    turnover     = models.Charfield(max_length = 50)
-    breakeven    = models.Charfield(max_length = 50)
-    places       = models.Charfield(max_length=2000)
+    manpower     = models.CharField(max_length = 100)
+    fertilizer   = models.CharField(max_length = 100)
+    insecticides = models.CharField(max_length = 50)
+    pesticides   = models.CharField(max_length = 50)
+    investment   = models.CharField(max_length = 50)
+    turnover     = models.CharField(max_length = 50)
+    breakeven    = models.CharField(max_length = 50)
+    places       = models.CharField(max_length=2000)
 
     class Meta:
         verbose_name = 'Crop'
